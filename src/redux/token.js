c259db5a58dce45b3e6ec361d049d30ce9d987cc
Venkata Seshaiah <nvseshaiah2013@gmail.com>
2024-03-27 23:58:23 +0530

    payload changes

--- src/redux/token.js
+++ src/redux/token.js
@@ -15,7 +15,7 @@ export const Token = (state =
             case TOKEN_LOADING : return { ...state, isLoading : true };
             case CLEAR_TOKEN : return { ...state, isLoading : false, secretToken : null };
             case GENERATE_TOKEN : return { ...state, isLoading : false, message : action.payload.message, status : action.payload.status, code : action.payload.payload.code };
-            case GET_TOKEN : return { ...state, isLoading : false, secretToken : action.payload.token, status : action.payload.status, message : action.payload.message };
+            case GET_TOKEN : return { ...state, isLoading : false, secretToken : action.payload.payload.token, status : action.payload.status, message : action.payload.message };
             case VALIDATE_BORROW : return { ...state, isLoading : false, status : action.payload.status, message : action.payload.message };
             case VALIDATE_RETURN : return { ...state, isLoading : false, status : action.payload.status, message : action.payload.message };
             case REJECT_BORROW : return {...state, isLoading : false, status : action.payload.status, message : action.payload.message };
